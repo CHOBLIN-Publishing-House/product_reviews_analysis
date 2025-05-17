23ee3b2663a8f2f58eb371de94caee8b6f8df793
CHOBLIN-Publishing-House <128604903+CHOBLIN-Publishing-House@users.noreply.github.com>
2025-05-17 14:31:05 +0300

    upd2

--- page_1.py
+++ page_1.py
@@ -2,15 +2,11 @@ import streamlit as st
 
 st.set_page_config(layout="wide")  # Это убирает боковые отступы
 
-
-st.title("Дашборд Yandex Datalens")
-
 st.markdown("""
 <iframe 
-    src="https://datalens.yandex/fqptgydqi0by1?_no_controls=1" 
-    width="100%" 
-    height="600px" 
+    src="https://datalens.yandex/u9en3kq1r9s8g?_no_controls=1" 
+    width="105%" 
+    height="780px" 
     frameborder="0">
 </iframe>
 """, unsafe_allow_html=True)
-
